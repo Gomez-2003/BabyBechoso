6b3d4db971803ad9d59d887ae0b6dfe4c075207e
thechoute <82709682+thechoute@users.noreply.github.com>
2021-10-25 17:40:33 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+echo -e '\e[1;32m
+╭━━╮   ╭╮     ╭━━╮      ╭╮           ╭━━━━┳╮    ╭━━━┳╮       ╭╮
+┃╭╮┃   ┃┃     ┃╭╮┃      ┃┃           ┃╭╮╭╮┃┃    ┃╭━╮┃┃      ╭╯╰╮
+┃╰╯╰┳━━┫╰━┳╮ ╭┫╰╯╰┳━━┳━━┫╰━┳━━┳━━┳━━╮╰╯┃┃╰┫╰━┳━━┫┃ ╰┫╰━┳━━┳╮┣╮╭╋━━╮
+┃╭━╮┃╭╮┃╭╮┃┃ ┃┃╭━╮┃┃━┫╭━┫╭╮┃╭╮┃━━┫╭╮┃  ┃┃ ┃╭╮┃┃━┫┃ ╭┫╭╮┃╭╮┃┃┃┃┃┃┃━┫
+┃╰━╯┃╭╮┃╰╯┃╰━╯┃╰━╯┃┃━┫╰━┫┃┃┃╰╯┣━━┃╰╯┃  ┃┃ ┃┃┃┃┃━┫╰━╯┃┃┃┃╰╯┃╰╯┃╰┫┃━┫
+╰━━━┻╯╰┻━━┻━╮╭┻━━━┻━━┻━━┻╯╰┻━━┻━━┻━━╯  ╰╯ ╰╯╰┻━━┻━━━┻╯╰┻━━┻━━┻━┻━━╯
+          ╭━╯┃
+          ╰━━╯'
+npm install
+npm install -g npm@6
+npm install toml
+npm i crypto-js
+npm i moment-timezone
+npm i axios
+npm i node-fetch@2.6.1 - npm
+npm i requests 
+npm install request --save
+npm i cfonts
+npm i spinnies
+npm i image-to-base64
+npm i remove.bg
+npm i fluent-ffmpeg
+npm start
